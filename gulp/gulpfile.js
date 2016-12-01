6e95f9b38c32bce314b4eba8167f35456a2f5434
okacunni <okacunni@gmail.com>
2016-12-01 23:02:54 +0900

    gulp タスクを追加

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -3,6 +3,7 @@
 var gulp    = require("gulp");
 var plumber = require("gulp-plumber");
 var rename  = require('gulp-rename');
+var header  = require('gulp-header');
 var compass = require("gulp-compass");
 var autoprefixer = require("gulp-autoprefixer");
 var minifyCss = require("gulp-minify-css");
@@ -15,12 +16,12 @@ var TUMBLR_DIR  = "tumblr.com/";
 var TUMBLR_ROOT_DIR = "../" + TUMBLR_DIR;
 var TUMBLR_SRC_SASS_DIR = SOURCE_DIR + TUMBLR_DIR + SASS_DIR;
 var TUMBLR_SRC_CSS_DIR  = SOURCE_DIR + TUMBLR_DIR + CSS_DIR;
-var TUMBLR_SCSS_FILE = TUMBLR_SRC_SASS_DIR + "*.scss";
-var TUMBLR_CSS_FILE  = TUMBLR_SRC_CSS_DIR + "*.css";
-var TUMBLR_CSS_DEST_DIR = TUMBLR_ROOT_DIR + CSS_DIR;
+var TUMBLR_SRC_SCSS_FILE = TUMBLR_SRC_SASS_DIR + "*.scss";
+var TUMBLR_SRC_CSS_FILE  = TUMBLR_SRC_CSS_DIR + "*.css";
+var TUMBLR_DEST_CSS_DIR  = TUMBLR_ROOT_DIR + CSS_DIR;
 
 gulp.task("compass-tumblr", function(){
-  gulp.src([TUMBLR_SCSS_FILE])
+  gulp.src([TUMBLR_SRC_SCSS_FILE])
     .pipe(plumber({
       errorHandler: function(error) {
         console.log(error.message);
@@ -31,21 +32,26 @@ gulp.task("compass-tumblr", function(){
       sass: TUMBLR_SRC_SASS_DIR,
       css: TUMBLR_SRC_CSS_DIR
     }))
-    .pipe(gulp.dest(TUMBLR_CSS_DEST_DIR))
+    .pipe(gulp.dest(TUMBLR_SRC_CSS_DIR))
     ;
 });
 
-gulp.task("minify-tumblr", function(){
-  gulp.src([TUMBLR_CSS_FILE])
-      .pipe(plumber())
-      .pipe(minifyCss({
-        'keepBreaks': false
-      }))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest(TUMBLR_CSS_DEST_DIR))
-      ;
+gulp.task("refine-tumblr", function(){
+  gulp.src(TUMBLR_SRC_CSS_FILE)
+    .pipe(plumber())
+    .pipe(header("@charset \"UTF-8\";\n\n"))
+    .pipe(minifyCss({
+      'keepBreaks': false
+    }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(TUMBLR_DEST_CSS_DIR))
+    ;
 });
 
-gulp.task("tumblr", function(){
-  gulp.watch([TUMBLR_SCSS_FILE], ["compass-tumblr", "minify-tumblr"]);
+gulp.task("build-tumblr", ["compass-tumblr", "refine-tumblr"], function(){
+  console.log("build complete");
+});
+
+gulp.task("watch", function(){
+  gulp.watch([TUMBLR_SRC_SCSS_FILE], ["build-tumblr"]);
 });
