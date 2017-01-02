a80c5c4ce1311d77c5e257973b91b1d286bc8f96
okacunni <okacunni@gmail.com>
2017-01-02 21:33:21 +0900

    fixedメニュー調整

--- tumblr.com/js/main.js
+++ tumblr.com/js/main.js
@@ -1,16 +1,23 @@
 $(function(){
+  var $win = $(window);
+  var $body = $('body');
   var $page = $('#page');
   var adjustHeight = $('#header').height();
   var $nav = $('.nav-wrapper');
   var offsetTop = $nav.offset().top;
   var isFixed = false;
-  $(window).scroll(function(){
-    isFixed = ($(window).scrollTop() > (offsetTop + $nav.height())) ? true : false;
+  $win.resize(function(){
+    adjustHeight = $('#header').height();
+    $page.css('padding-top', 0);
+  });
+  $win.scroll(function(){
+    if ($win.width() > 736) return false;
+    isFixed = ($win.scrollTop() > (offsetTop + $nav.height())) ? true : false;
     if (isFixed) {
       $page.css('padding-top', adjustHeight);
     } else {
       $page.css('padding-top', 0);
     }
-    $page.toggleClass('fixed-header', isFixed);
+    $body.toggleClass('fixed-header', isFixed);
   });
 });
